3565ea4ae2c5ecc5a9853daee8df248b6fd0a9b8
mac <mac@Andre-Baboo-Kos.local>
2026-07-16 09:14:50 +0700

    perbaikan

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.babookos.com',
+          },
+        ],
+        destination: 'https://babookos.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
